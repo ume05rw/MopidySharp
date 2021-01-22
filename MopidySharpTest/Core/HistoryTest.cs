ec665c8a525fbd4e51ba0d736c3d466263fd6ecc
ume05rw <ume05rw@gmail.com>
2021-01-22 09:57:38 +0900

    不要な参照を削除。

--- MopidySharpTest/Core/HistoryTest.cs
+++ MopidySharpTest/Core/HistoryTest.cs
@@ -1,8 +1,7 @@
-using System;
-using System.Threading.Tasks;
-using Xunit;
 using Mopidy.Core;
 using MopidySharpTest.Bases;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace MopidySharpTest.Core
 {
