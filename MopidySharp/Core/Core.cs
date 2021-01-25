8cff70a377ed3ab1cadd44e421da3f080cea6314
ume05rw <ume05rw@gmail.com>
2021-01-25 15:05:34 +0900

    Mopidy.Core.Coreクラスを追加。Mopidy側の名前空間と異なるが、名前空間Coreとクラス名Coreを共存できず、Core.Coreとした。特段重要度の高いメソッドもないため、まあよしとする。

---
+++ MopidySharp/Core/Core.cs
@@ -0,0 +1,60 @@
+﻿using Mopidy.Models.JsonRpcs;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Mopidy.Core
+{
+    public static class Core
+    {
+        private const string MethodGetUriSchemes = "core.get_uri_schemes";
+        private const string MethodGetVersion = "core.get_version";
+
+        private static readonly Query _query = Query.Get();
+
+        /// <summary>
+        /// Get list of URI schemes we can handle
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<(bool Succeeded, string[] Result)> GetUriSchemes()
+        {
+            var request = JsonRpcFactory.CreateRequest(Core.MethodGetUriSchemes);
+
+            var response = await Core._query.Exec(request);
+
+            if (response.Error != null)
+            {
+                // TODO: Logger実装, Logger.Write(response.Error);
+                return (false, null);
+            }
+
+            // 戻り値の型は、[ JObject | JArray | JValue | null ] のどれか。
+            // 型が違うとパースエラーになる。
+            var result = JArray.FromObject(response.Result).ToObject<string[]>();
+
+            return (true, result);
+        }
+
+        /// <summary>
+        /// Get version of the Mopidy core API
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<(bool Succeeded, string Result)> GetVersion()
+        {
+            var request = JsonRpcFactory.CreateRequest(Core.MethodGetVersion);
+
+            var response = await Core._query.Exec(request);
+
+            if (response.Error != null)
+            {
+                // TODO: Logger実装, Logger.Write(response.Error);
+                return (false, null);
+            }
+
+            // 戻り値の型は、[ JObject | JArray | JValue | null ] のどれか。
+            // 型が違うとパースエラーになる。
+            var result = JValue.FromObject(response.Result).ToObject<string>();
+
+            return (true, result);
+        }
+    }
+}
---
+++ MopidySharpTest/Core/CoreTest.cs
@@ -0,0 +1,25 @@
+﻿using MopidySharpTest.Bases;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MopidySharpTest.Core
+{
+    public class CoreTest : TestBase
+    {
+        [Fact]
+        public async Task GetUriSchemesTest()
+        {
+            var res1 = await Mopidy.Core.Core.GetUriSchemes();
+            Assert.True(res1.Succeeded);
+            Assert.True(0 < res1.Result.Length);
+        }
+
+        [Fact]
+        public async Task GetVersionTest()
+        {
+            var res1 = await Mopidy.Core.Core.GetVersion();
+            Assert.True(res1.Succeeded);
+            Assert.NotNull(res1.Result);
+        }
+    }
+}
